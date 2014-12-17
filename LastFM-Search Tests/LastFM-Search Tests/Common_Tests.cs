148f56c5d8af0fbefb95d094c4d095ab166c3e9c
Peter Curd <pc@airinmar.com>
2014-12-17 18:44:19 +0000

    Initial Project

---
+++ LastFM-Search Tests/LastFM-Search Tests/Common_Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LastFM_Search_Tests
+{
+    [TestClass]
+    public class Common_Tests
+    {
+        [TestMethod]
+        public void Test_AlwaysPasses()
+        {
+            Assert.AreEqual(1,1);
+        }
+    }
+}
